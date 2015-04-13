064f13a66e5297810d8f949a39aaca98ed0e499c
isabisa <alisa.r.herr@gmail.com>
2015-04-13 14:52:23 -0400

    Add Disqus for comments. Style posts front end & editor in back end to match.

--- wp-content/themes/ncinsight/templates/content-post.php
+++ wp-content/themes/ncinsight/templates/content-post.php
@@ -1,14 +1,5 @@
 <?php
-$page = get_query_var('page');
-
 $comments_open = comments_open();
-
-$author_id = get_the_author_meta('ID');
-$author_bio = get_posts(array('post_type' => 'bio', 'meta_key' => 'user', 'meta_value' => $author_id));
-$author_avatar = get_field('avatar', $author_bio[0]->ID);
-$author_avatar_sized = mr_image_resize($author_avatar, 140, null, false, '', false);
-
-$column = wp_get_post_terms(get_the_id(), 'column');
 
 $category = get_the_category();
   // Convert category results to array instead of object
@@ -18,114 +9,31 @@ $category = get_the_category();
   $cats_hide = array();
   // Determine array indexes for labels we don't want to show
   $cats_hide[] = array_search('Uncategorized', array_column($category, 'cat_name'));
-  $cats_hide[] = array_search('Hide from home', array_column($category, 'cat_name'));
-  $cats_hide[] = array_search('Hide from archives', array_column($category, 'cat_name'));
   // Remove empty results
   $cats_hide = array_filter($cats_hide, 'strlen');
 
 ?>
 
 <article <?php post_class('article'); ?>>
-  <?php
-  $featured_image_align = get_field('featured_image_alignment');
-
-  if (has_post_thumbnail() && $featured_image_align == 'hero') { ?>
-    <header class="entry-header photo-overlay">
-      <?php the_post_thumbnail(); ?>
-      <div class="article-title-overlay">
-        <div class="container">
-          <div class="row">
-            <div class="col-md-9 col-centered">
-              <?php
-              if ($column) {
-                ?>
-                <span class="label"><?php echo $column[0]->name; ?></span>
-                <?php
-              } else {
-                // Only show label of category if it's not in above list
-                foreach ($category as $key=>$value) {
-                  if (!in_array($key, $cats_hide)) {
-                    echo '<span class="label">' . $value['cat_name'] . '</span> ';
-                  }
-                }
-              }
-              ?>
-              <h1 class="entry-title"><?php the_title(); ?></h1>
-              <?php get_template_part('templates/entry-meta'); ?>
-            </div>
-          </div>
-        </div>
-      </div>
-    </header>
-  <?php } else {
-    if ($column) {
-      ?>
-      <div class="column-banner <?php echo $column[0]->slug; ?>">
-        <div class="container">
-          <div class="row">
-            <div class="col-md-9 col-centered">
-              <div class="column-name"><?php echo $column[0]->name; ?></div>
-              <?php if ($author_avatar) { ?>
-                <div class="avatar hidden-xs">
-                  <img src="<?php echo $author_avatar_sized['url']; ?>" alt="<?php the_author(); ?>" />
-                </div>
-              <?php } ?>
-            </div>
-          </div>
-        </div>
-      </div>
-      <?php
-    }
-    ?>
     <header class="entry-header container">
       <div class="row">
         <div class="col-md-9 col-centered">
           <?php
-          if ($column) {
-            ?>
-            <span class="label"><?php echo $column[0]->name; ?></span>
-            <?php
-          } else {
-            $cats_hide = array();
-            // Determine array indexes for labels we don't want to show
-            $cats_hide[] = array_search('Uncategorized', array_column($category, 'cat_name'));
-            $cats_hide[] = array_search('Hide from home', array_column($category, 'cat_name'));
-            $cats_hide[] = array_search('Hide from archives', array_column($category, 'cat_name'));
-            // Remove empty results
-            $cats_hide = array_filter($cats_hide, 'strlen');
-
-            // Only show label of category if it's not in above list
-            foreach ($category as $key=>$value) {
-              if (!in_array($key, $cats_hide)) {
-                echo '<span class="label">' . $value['cat_name'] . '</span> ';
-              }
+          // Only show label of category if it's not in hidden list
+          foreach ($category as $key=>$value) {
+            if (!in_array($key, $cats_hide)) {
+              echo '<span class="label">' . $value['cat_name'] . '</span> ';
             }
           }
           ?>
 
-          <?php
-          if (in_category('109')) {  // 1868 Constitutional Convention
-            ?>
-            <div class="top-margin">
-              <p><?php echo category_description(109); ?></p>
-            </div>
-            <div class="row bottom-margin">
-              <div class="col-md-6">
-                <?php previous_post_link('%link', '&laquo; Previous day', true, 'category'); ?>
-              </div>
-              <div class="col-md-6 text-right">
-                <?php next_post_link('%link', 'Next day &raquo;', true, 'category'); ?>
-              </div>
-            </div>
-            <?php
-          }
-          ?>
-
           <h1 class="entry-title"><?php the_title(); ?></h1>
+
           <?php get_template_part('templates/entry-meta'); ?>
           <?php get_template_part('templates/social', 'share'); ?>
+
           <?php
-          if (has_post_thumbnail() && $featured_image_align != 'none') {
+          if (has_post_thumbnail()) {
             the_post_thumbnail('large');
             $thumb_id = get_post_thumbnail_id();
             $thumb_post = get_post($thumb_id);
@@ -139,37 +47,11 @@ $category = get_the_category();
         </div>
       </div>
     </header>
-  <?php } ?>
-
-  <?php if (get_field('longform_intro') && $page < 2) { ?>
-    <div class="longform-intro">
-      <div class="container">
-        <div class="row">
-          <div class="col-md-9 col-centered intro">
-            <?php the_field('longform_intro'); ?>
-          </div>
-        </div>
-      </div>
-    </div>
-  <?php } ?>
 
     <div class="entry-content container">
       <div class="row">
         <div class="col-lg-7 col-md-9 col-centered">
-          <?php if (has_post_thumbnail() && $featured_image_align == 'hero') {
-            get_template_part('templates/social', 'share');
-          } ?>
-
           <?php the_content(); ?>
-
-          <?php
-          wp_link_pages(
-            array(
-              'before' => '<nav class="page-nav"><p><span class="pages">Skip to page:</span>',
-              'after' => '</p></nav>'
-            )
-          );
-          ?>
 
           <?php get_template_part('templates/social', 'share'); ?>
 
@@ -196,181 +78,11 @@ $category = get_the_category();
     <footer class="container">
       <?php if ($comments_open == 1) { ?>
         <div class="row">
-          <div class="col-lg-7 col-md-9 col-centered">
-            <h3>About <?php the_author(); ?></h3>
-            <?php
-            $args = array(
-              'post_type' => 'bio',
-              'meta_query' => array(
-                array(
-                  'key' => 'user',
-                  'value' => $author_id
-                )
-              )
-            );
-
-            $bio = new WP_Query($args);
-
-            if ($bio->have_posts()) : while ($bio->have_posts()) : $bio->the_post(); ?>
-
-              <div class="row">
-                <div class="col-xs-5 col-sm-3">
-                  <?php the_post_thumbnail('bio-headshot'); ?>
-                </div>
-
-                <div class="col-xs-7 col-sm-9">
-                  <?php get_template_part('templates/author', 'excerpt'); ?>
-                </div>
-              </div>
-
-            <?php endwhile; endif; wp_reset_query(); ?>
+          <div class="col-lg-7 col-md-9 col-centered hidden-print">
+            <h3>Leave a comment</h3>
+            <?php comments_template('templates/comments'); ?>
           </div>
         </div>
       <?php } ?>
-
-      <div class="row">
-        <?php if ($comments_open == 1) { ?>
-          <div class="col-md-8 col-lg-7 hidden-print">
-            <h3>Join the conversation</h3>
-            <?php comments_template('templates/comments'); ?>
-          </div>
-
-          <div class="col-md-4 col-lg-push-1">
-        <?php } else { ?>
-
-          <?php if (in_category('109')) { // 1868 ?>
-            <div class="hidden-sm hidden-xs col-md-2"></div>
-          <?php } ?>
-
-          <div class="col-sm-12 col-md-4">
-            <h3>About <?php the_author(); ?></h3>
-            <?php
-            $args = array(
-              'post_type' => 'bio',
-              'meta_query' => array(
-                array(
-                  'key' => 'user',
-                  'value' => $author_id
-                )
-              )
-            );
-
-            $bio = new WP_Query($args);
-
-            if ($bio->have_posts()) : while ($bio->have_posts()) : $bio->the_post(); ?>
-              <?php the_post_thumbnail('bio-headshot', array('class' => 'author-photo')); ?>
-              <?php get_template_part('templates/author', 'excerpt'); ?>
-            <?php endwhile; endif; wp_reset_query(); ?>
-
-          </div>
-
-          <?php if (!in_category('109')) { // 1868 ?>
-            <div class="col-sm-6 col-md-4 hidden-print">
-          <?php } ?>
-
-        <?php } ?>
-
-        <?php if (!in_category('109')) { // 1868 ?>
-
-          <?php if ($comments_open == 1) { ?>
-            <div class="col-sm-6 col-md-12 hidden-print">
-          <?php } ?>
-
-          <h3>Recommended for you</h3>
-          <?php
-          $recommended = get_field('recommended_articles');
-          if ($recommended) {
-            // set this to only display first one for now.
-            // TODO: add some way to have more than 1 recommended article
-            $post = $recommended[0];
-
-            $pid = $post->ID;
-
-            setup_postdata($post);
-
-            $author_id = get_the_author_meta('ID');
-            $author_bio = get_posts(array('post_type' => 'bio', 'meta_key' => 'user', 'meta_value' => $author_id));
-
-            $category = get_the_category($pid);
-            if (has_post_thumbnail()) {
-              $image_id = get_post_thumbnail_id();
-              $image_url = wp_get_attachment_image_src($image_id, 'featured-thumbnail');
-              $image_sized['url'] = $image_url[0];
-            } else {
-              $image_src = catch_that_image();
-              $image_sized = mr_image_resize($image_src, 295, 295, true, false);
-            }
-            ?>
-            <div class="has-photo-overlay">
-              <div class="photo-overlay">
-                <span class="label"><?php if ($post->post_type == 'map') { echo 'Map'; } else { if ($category[0]->cat_name != 'Uncategorized' && $category[0]->cat_name != 'Hide from home') { echo $category[0]->cat_name; }} ?></span>
-                <h2 class="post-title"><?php echo $post->post_title; ?></h2>
-                <p class="meta">by <?php echo get_the_author_meta('display_name', $post->post_author); ?> on <date><?php echo date(get_option('date_format'), strtotime($post->post_date)); ?></date></p>
-                <a class="mega-link" href="<?php the_permalink(); ?>"></a>
-                <?php if ($image_sized['url']) { ?>
-                <img src="<?php echo $image_sized['url']; ?>" />
-                <?php } ?>
-              </div>
-            </div>
-            <?php
-            wp_reset_postdata();
-          } else {
-            // previous post by same author
-            $post = get_adjacent_author_post(true);
-
-            $pid = $post->ID;
-
-            setup_postdata($post);
-
-            $author_id = get_the_author_meta('ID');
-            $author_bio = get_posts(array('post_type' => 'bio', 'meta_key' => 'user', 'meta_value' => $author_id));
-            $author_type = wp_get_post_terms($author_bio[0]->ID, 'author-type');
-
-            $category = get_the_category($pid);
-            if (has_post_thumbnail()) {
-              $image_id = get_post_thumbnail_id();
-              $image_url = wp_get_attachment_image_src($image_id, 'featured-thumbnail');
-              $image_sized['url'] = $image_url[0];
-            } else {
-              $image_src = catch_that_image();
-              $image_sized = mr_image_resize($image_src, 295, 295, true, false);
-            }
-            ?>
-            <div class="has-photo-overlay">
-              <div class="photo-overlay">
-                <span class="label"><?php if ($post->post_type == 'map') { echo 'Map'; } else { if (is_singular('feature')) { echo $author_type[0]->name; } elseif ($category[0]->cat_name != 'Uncategorized' && $category[0]->cat_name != 'Hide from home') { echo $category[0]->cat_name; }} ?></span>
-                <h2 class="post-title"><?php echo $post->post_title; ?></h2>
-                <p class="meta">by <?php echo get_the_author_meta('display_name', $post->post_author); ?> on <date><?php echo date(get_option('date_format'), strtotime($post->post_date)); ?></date></p>
-                <a class="mega-link" href="<?php the_permalink(); ?>"></a>
-                <?php if ($image_sized['url']) { ?>
-                <img src="<?php echo $image_sized['url']; ?>" />
-                <?php } ?>
-              </div>
-            </div>
-            <?php
-            wp_reset_postdata();
-          }
-          ?>
-
-          </div>
-
-        <?php } ?>
-
-        <?php if ($comments_open == 1) { ?>
-        <div class="col-sm-6 col-md-12 hidden-print">
-        <?php } else { ?>
-        <div class="col-sm-6 col-md-4 hidden-print">
-        <?php } ?>
-
-          <h3>Stay connected</h3>
-          <?php get_template_part('templates/email-signup'); ?>
-
-        </div>
-
-        <?php if (in_category('109')) { // 1868 ?>
-          <div class="hidden-sm hidden-xs col-md-2"></div>
-        <?php } ?>
-
-      </div>
     </footer>
 </article>
--- wp-content/themes/ncinsight/templates/entry-meta.php
+++ wp-content/themes/ncinsight/templates/entry-meta.php
@@ -1,9 +1,5 @@
 <p class="byline author vcard">
-  by
-  <a href="<?php echo get_author_posts_url(get_the_author_meta('ID')); ?>" rel="author" class="fn">
-    <?php echo get_the_author(); ?>
-  </a>
-  on
+  by <?php the_field('author'); ?> on
   <time class="published" datetime="<?php echo get_the_time('c'); ?>">
     <?php the_time(get_option('date_format')); ?>
   </time>
